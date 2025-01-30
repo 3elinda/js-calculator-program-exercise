314c1f3b7c94f0b1db6ecff2db86d007601476f8
3elinda <belinda.cedillo_santiago24@student.cart.org>
2025-01-29 23:16:16 -0800

    calculator-program-exercise

--- calculator.js
+++ calculator.js
@@ -1,3 +1,35 @@
-function calculation() {
-    let number = prompt("")
+const calculateButton = document.getElementById("calculate-btn");
+calculateButton.addEventListener("click", promptUser);
+
+function promptUser() {
+  // parseFLoat means string is converted to decimal
+  // number and stops reading non-numeric
+  // characters except for . or e
+
+  let a = parseFloat(prompt("Insert first number"));
+  let operator = prompt("Insert (+, -, ×, ÷) operator");
+  let b = parseFloat(prompt("Insert second number"));
+  let answer = calculate(a, b, operator);
+  document.write(answer);
+
+  //   let userInput = prompt("Insert operation");
+  //   console.log(typeof userInput);
+}
+
+function calculate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "÷":
+      if (b === 0) {
+        return "undefined";
+      }
+      return a / b;
+    default:
+      return "";
+  }
 }
